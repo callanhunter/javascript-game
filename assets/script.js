8e4a95d5cff3aafce4e219d1884e4e48f6f7d954
callanhunter <callan57hunter@gmail.com>
2022-04-05 22:30:54 -0700

    updating javascript

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,9 @@ var nextButton = document.getElementById("next-button");
 var currentQuestion = {};
 var score = 0;
 var questionCount = 0;
+var maxQuestions = 4;
+var maxScore = 0;
+var questionsLeft = 0;
 // var number = document.getElementById("message");
 
 // creating start button that will bring up the next button
@@ -15,9 +18,20 @@ function startGame() {
 };
 
 function nextQuestion() {
+    score =0;
+    questionCount = 0;
+    questionsLeft = [...questions];
 }
 
-var question = [
+function anotherQuestion() {
+    if (questionsLeft === 0) {
+        localStorage.setItem("recentScore", score)
+    }
+
+    questionCount++;
+    questions.innerText = currentQuestion.questions;
+
+var questions = [
     {
         question1: "What is my name?",
         choiceA: "Ron",
