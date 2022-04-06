19d493266c9a1abcde44ff10d727f384f738a9bd
callanhunter <callan57hunter@gmail.com>
2022-04-05 22:12:55 -0700

    style changes and functions added

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,62 @@
-var element = document.getElementById("start-button");
-var number = document.getElementById("message");
+var startButton = document.getElementById("start-button");
+var nextButton = document.getElementById("next-button");
+var currentQuestion = {};
+var score = 0;
+var questionCount = 0;
+// var number = document.getElementById("message");
 
-element.addEventListener("click", function() {
-    console.log(javascriptQuestions.question1)
-});
+// creating start button that will bring up the next button
+startButton.addEventListener("click", startGame);
+    // document.getElementById("next-button");
 
-var javascriptQuestions = {
-    question1: "What is my middle name?",
+
+function startGame() {
+    nextQuestion()
+};
+
+function nextQuestion() {
 }
+
+var question = [
+    {
+        question1: "What is my name?",
+        choiceA: "Ron",
+        choiceB: "Callan",
+        choiceC: "Todd",
+        choiceD: "Keal",
+        correctAnswer: 2,
+    },
+    {
+        question2: "What is my name?",
+        choiceA: "Ron",
+        choiceB: "Callan",
+        choiceC: "Todd",
+        choiceD: "Keal",
+        correctAnswer: 2,
+    },
+    {
+        question3: "What is my name?",
+        choiceA: "Ron",
+        choiceB: "Callan",
+        choiceC: "Todd",
+        choiceD: "Keal",
+        correctAnswer: 2,
+    },
+    {
+        question4: "What is my name?",
+        choiceA: "Ron",
+        choiceB: "Callan",
+        choiceC: "Todd",
+        choiceD: "Keal",
+        correctAnswer: 2,
+    },
+    {
+        question5: "What is my name?",
+        choiceA: "Ron",
+        choiceB: "Callan",
+        choiceC: "Todd",
+        choiceD: "Keal",
+        correctAnswer: 2,
+    },
+]
+
